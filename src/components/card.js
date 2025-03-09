75faa50c864b6b8a7dab4f6b29bba612a2cc4764
KingViktorNik <viktor.korol31@yandex.ru>
2025-03-09 16:50:09 +0600

    реализация функции _Лайк карточки_

--- src/components/card.js
+++ src/components/card.js
@@ -16,12 +16,14 @@ export function addCard(card, deleteCard) {
   const placesItem = cardElement.querySelector('.places__item');
   const cardDeleteButton = placesItem.querySelector('.card__delete-button');
   const cardImage = placesItem.querySelector('.card__image');
+  const likeButton = cardElement.querySelector('.card__like-button');
 
   placesItem.querySelector('.card__title').textContent = card.name;
   cardImage.src = card.link;
   cardImage.alt = card.alt;
   cardDeleteButton.addEventListener('click', deleteCard);
   cardImage.addEventListener('click', openPopup);
+  likeButton.addEventListener('click', likeToggle);
 
   return placesItem;
 }
@@ -30,4 +32,15 @@ export function addCard(card, deleteCard) {
 
 export function deleteCard(event) {
   event.target.parentElement.remove();
+}
+
+function likeToggle(evt) {
+  const classLikeActive = 'card__like-button_is-active';
+
+  if (evt.target.classList.contains(classLikeActive)) {
+    evt.target.classList.remove(classLikeActive)
+    return
+  } 
+
+  evt.target.classList.add(classLikeActive);
 }
