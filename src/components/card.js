bcf6faad638666c311da0ba06cb795d801c102a9
KingViktorNik <viktor.korol31@yandex.ru>
2025-03-25 09:05:32 +0600

    Интеграция с API

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,42 @@
 // @todo: Функция создания карточки
 
-export function createCard(card, deleteCard, likeToggle, openModal) {
+export function createCard(card, userId, deleteCard, lickClick, openModalImage, openModalDeleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.cloneNode(true);
   const placesItem = cardElement.querySelector('.places__item');
-  
+
   const cardImage = placesItem.querySelector('.card__image');
+  const cardLikes = placesItem.querySelector('.card__like');
 
   const cardDeleteButton = placesItem.querySelector('.card__delete-button');
   const likeButton = cardElement.querySelector('.card__like-button');
-  
+
   placesItem.querySelector('.card__title').textContent = card.name;
+  placesItem.querySelector('.card__like-counter').textContent = card.likeCounter;
   cardImage.src = card.link;
   cardImage.alt = card.alt;
 
-  cardDeleteButton.addEventListener('click', () => deleteCard(placesItem));
-  likeButton.addEventListener('click', likeToggle);
-  cardImage.addEventListener('click', openModal);
+  if (userId !== card.idAuthor) {
+    cardDeleteButton.classList.add('card__delete-button-hide');
+    
+  } else {
+    cardDeleteButton.classList.remove('card__delete-button-hide');
+  }
+
+  if (card.like) {
+    likeButton.classList.add('card__like-button_is-active');
+  }
+
+  cardDeleteButton.addEventListener('click', () => openModalDeleteCard({card, placesItem}));
+  likeButton.addEventListener('click', evt => {
+    lickClick(card, cardLikes);
+    if (card.like) {
+      card.like = false;
+    } else {
+      card.like = true;
+    }
+  });
+  cardImage.addEventListener('click', openModalImage);
 
   return placesItem;
 }
@@ -29,7 +49,10 @@ export function deleteCard(card) {
 
 // @todo: Функция лайка карточки
 
-export function likeToggle(evt) {
+export function likeToggle(cardLikes, likes) {
   const classLikeActive = 'card__like-button_is-active';
-  evt.target.classList.toggle(classLikeActive);
+  const classLikeCounter = '.card__like-counter';
+  const buttonLike = cardLikes.querySelector('.card__like-button');
+  buttonLike.classList.toggle(classLikeActive);
+  cardLikes.querySelector(classLikeCounter).textContent = likes;
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import { data } from 'autoprefixer';
 import '../pages/index.css';
 import { createCard, deleteCard, likeToggle } from './card.js';
 import { initialCards } from './cards.js';
@@ -6,6 +7,11 @@ import { enableValidation, toggleButtonState } from './validation.js';
 
 const pageСontent = document.querySelector('.page__content');
 const placesList = document.querySelector('.places__list');
+
+const user = {
+  token: '9a6aeb5f-5827-4768-8d6b-54e318fb40d2',
+  cohortId: 'wff-cohort-35',
+};
 
 const  validationConfig = {
   formSelector: '.popup__form',
@@ -17,8 +23,11 @@ const  validationConfig = {
 };
 
 const profile = {
+  id: null,
+  cohort: null,
   name: pageСontent.querySelector('.profile__title'),
   job: pageСontent.querySelector('.profile__description'),
+  avatar: pageСontent.querySelector('.profile__image'),
 }
 
 // Переменные форм
@@ -43,6 +52,11 @@ const buttonSaveProfile = modalEditProfile.querySelector('.popup__button');
 const modalNewCard = pageСontent.querySelector('.popup_type_new-card');
 const buttonSaveCard = modalNewCard.querySelector('.popup__button');
 
+// Переменные модального окна удаления карточки
+
+const modalDeleteCard = pageСontent.querySelector('.popup_type_delete-card');
+const buttonDeleteCard = modalDeleteCard.querySelector('.popup__button');
+
 // Переменные модального окна просмотра изображения
 
 const popupTypeImageCard = pageСontent.querySelector('.popup_type_image');
@@ -62,8 +76,33 @@ function openAddCardModal(evt) {
     link: linkInput.value,
     alt: nameInput.value,
   }
-  
-  placesList.prepend(createCard(card, deleteCard, likeToggle, openModalImageCard));
+  fetch(`https://nomoreparties.co/v1/${user.cohortId}/cards`, {
+    method: 'POST',
+    headers: {
+      authorization: user.token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: card.name,
+      link: card.link
+    })
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    }
+  })
+  .then(data => {
+    card.id = data['_id'];
+    card.name = data.name;
+    card.link = data.link;
+    card.alt = data.link;
+    card.likeCounter = data.likes.length;
+    card.idAuthor = data.owner['_id'];
+    placesList.prepend(createCard(card, profile.id, deleteCard, lickClick, openModalImageCard, openModalDeleteCard));
+  });
   
   nameInput.value = '';
   linkInput.value = '';
@@ -75,9 +114,29 @@ function openAddCardModal(evt) {
 
 function handleSubmitProfile(evt) {
   evt.preventDefault();
-
-  profile.name.textContent = formProfile.name.value.trim();
-  profile.job.textContent = formProfile.description.value.trim();
+  
+  fetch(`https://nomoreparties.co/v1/${user.cohortId}/users/me`, {
+    method: 'PATCH',
+    headers: {
+      authorization: user.token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: formProfile.name.value.trim(),
+      about: formProfile.description.value.trim()
+    })
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    }
+  })
+  .then(data => {
+    profile.name.textContent = data.name;
+    profile.job.textContent = data.about;
+  });
 
   closeModal();
 }
@@ -98,7 +157,27 @@ function openModalImageCard(evt) {
   openModal(popupTypeImageCard);
 }
 
-function clearValidation (form, config) {
+// Функция модального окна удаление карточки
+
+let handleSubmitConfirmPopup = null;
+function openModalDeleteCard(card) {
+  handleSubmitConfirmPopup = card;
+  openModal(modalDeleteCard);
+}
+
+buttonDeleteCard.addEventListener('click', () => {
+  console.log(`удаляем карточку ${handleSubmitConfirmPopup.card.id}`);
+  deleteCard(handleSubmitConfirmPopup.placesItem);
+  fetch(`https://nomoreparties.co/v1/${user.cohortId}/cards/${handleSubmitConfirmPopup.card.id}`, {
+    method: 'DELETE',
+    headers: {
+      authorization: user.token
+    }
+  })
+  closeModal();
+});
+
+function clearValidation(form, config) {
   const inputList = Array.from(form.querySelectorAll(config.inputSelector));
   const spanList = Array.from(form.querySelectorAll(`.${config.errorClass}`));
   const buttonElement = form.querySelector(config.submitButtonSelector);
@@ -132,7 +211,105 @@ buttonSaveCard.addEventListener('click', openAddCardModal);
 
 buttonsCloseModal.forEach(button => button.addEventListener('click', closeModal));
 
-initialCards.forEach(card => placesList.append(createCard(card, deleteCard, likeToggle, openModalImageCard)));
+// initialCards.forEach(card => placesList.append(createCard(card, user.id, deleteCard, likeToggle, openModalImageCard)));
 
 // Вызовем функцию волидации форм
 enableValidation(validationConfig);
+
+fetch(`https://nomoreparties.co/v1/${user.cohortId}/users/me`, {
+  method: 'GET',
+  headers: {
+    authorization: user.token
+  }
+})
+.then(res => {
+  if (res.ok) {
+    return res.json()
+  } else {
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  }
+})
+.then(data => {
+  const profile = {
+    name: data.name,
+    about: data.about,
+    avatar: data.avatar,
+    id: data['_id'],
+    cohort: data.cohort
+  }
+  setProfile(profile);
+})
+.catch(err => console.log('Ошибка. Запрос не выполнен: ', err));
+
+fetch(`https://nomoreparties.co/v1/${user.cohortId}/cards`, {
+  method: 'GET',
+  headers: {
+    authorization: user.token
+  }
+})
+.then(res => {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  }
+})
+.then(data => 
+  data.forEach (elemetnt => {
+    const card = {
+      id: elemetnt['_id'],
+      name: elemetnt.name,
+      link: elemetnt.link,
+      alt: elemetnt.name,
+      likeCounter: elemetnt.likes.length,
+      idAuthor:elemetnt.owner['_id'],
+      like: Array.from(elemetnt.likes).some(like => like['_id'] === profile.id)
+    }
+    // const hasInvalidInput = (inputList) => inputList.some(inputElement => !inputElement.validity.valid);
+    placesList.append(createCard(card, profile.id, deleteCard, lickClick, openModalImageCard, openModalDeleteCard));
+  })  
+) // если мы попали в этот then, data — это объект
+.catch(err => console.log('Ошибка. Запрос не выполнен: ', err));
+
+function setProfile({ name, about, avatar, id, cohort }) {
+  profile.id = id;
+  profile.name.textContent = name;
+  profile.job.textContent = about;
+  profile.avatar.src = avatar;
+  profile.cohort = cohort;
+}
+
+function lickClick(card, cardLikes) {
+  if (!card.like) {  
+    fetch(`https://nomoreparties.co/v1/${user.cohortId}/cards/likes/${card.id}`, {
+      method: 'PUT',
+      headers: {
+        authorization: user.token
+      }
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Что-то пошло не так: ${res.status}`);
+      }
+    })
+    .then(data => likeToggle(cardLikes, data.likes.length));
+  } else {
+    fetch(`https://nomoreparties.co/v1/${user.cohortId}/cards/likes/${card.id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: user.token
+      }
+    })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Что-то пошло не так: ${res.status}`);
+      }
+    })
+    .then(data => likeToggle(cardLikes, data.likes.length));
+  }
+  
+}
